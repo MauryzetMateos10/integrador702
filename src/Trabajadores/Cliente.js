e30a7c7a861b02ac02e027324ba78874b147137d
MauryzetMateos10 <152357902+MauryzetMateos10@users.noreply.github.com>
2024-12-01 18:18:36 -0600

    Update Cliente.js

--- src/Trabajadores/Cliente.js
+++ src/Trabajadores/Cliente.js
@@ -31,7 +31,7 @@ const Cliente = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch('https://alex.starcode.com.mx/apiBD.php');
+                const response = await fetch('https://alex.starcode.com.mx/apiAlumnos.php');
                 if (!response.ok) throw new Error('Network response was not ok');
                 
                 const data = await response.json();
